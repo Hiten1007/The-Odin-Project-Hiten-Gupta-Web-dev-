8933624fe025d399b7c32f987b7502fd87e70f26
Hiten1007 <hitengupta1005@gmail.com>
2024-10-19 19:11:18 +0530

    v0_1

--- RestaurantPage/src/Routes/about.js
+++ RestaurantPage/src/Routes/about.js
@@ -7,7 +7,7 @@ export const aboutUs = (function() {
 
         content.appendChild(menu.createHeading("About Us", "h2"));
 
-        content.appendChild(createText("Welcome to <strong>Taste Haven</strong>, a place where culinary passion meets exceptional flavors. Established in 2010, we have been serving our customers with an array of delicious dishes, crafted from the freshest ingredients and inspired by global cuisines. Our aim is to provide not just a meal, but an experience that delights your taste buds and creates unforgettable memories."));
+        content.appendChild(createText("Welcome to Taste Haven, a place where culinary passion meets exceptional flavors. Established in 2010, we have been serving our customers with an array of delicious dishes, crafted from the freshest ingredients and inspired by global cuisines. Our aim is to provide not just a meal, but an experience that delights your taste buds and creates unforgettable memories."));
         content.appendChild(createText("Our chefs are dedicated to bringing innovation and creativity to each dish, ensuring that every visit to Taste Haven offers something new and exciting. Whether you are here for a romantic dinner, a family gathering, or a casual meal with friends, our warm and cozy ambiance makes you feel right at home."));
         content.appendChild(createText(" We believe in the magic of great food and its ability to bring people together. Join us at Taste Haven and be a part of our story!"));
 
